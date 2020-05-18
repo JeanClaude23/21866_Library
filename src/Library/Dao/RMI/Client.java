b7b350740e4fc5ad1b33304f3d6e234f1854a1da
JeanClaude23 <62624966+JeanClaude23@users.noreply.github.com>
2020-05-18 19:25:53 +0200

    This is Lirary Project

--- src/Library/Dao/RMI/Client.java
+++ src/Library/Dao/RMI/Client.java
@@ -5,10 +5,29 @@
  */
 package Library.Dao.RMI;
 
+import Library.Service.IClientService;
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author jean claude
  */
 public class Client {
-    
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 2000);
+            IClientService clientservice = (IClientService) registry.lookup("clientservice");
+            boolean result = clientservice.save("0001", "kodo", "desire", "0789898989", "kodo@gmail.com", "Student", "C:\\Users\\jean claude\\Pictures\\Saved Pictures\\New folder\\black-r1-5784");
+            System.out.println(result ? "Saved Successfully" : "Error. can't saved");
+        } catch (RemoteException | NotBoundException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 }
